5a68ef0269fc8cebbd7db51dcc6ac8ad3c22bd3c
ArkannTV <115234074+ArkannTV@users.noreply.github.com>
2023-01-22 19:05:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 let i = 0
 const homer = document.querySelector(".homer")
 
-const interval = setInterval(() => {
-    homer.style.transform = `translateX(${i}px)`
-    i++
+// const interval = setInterval(() => {
+//     homer.style.transform = `translateX(${i}px)`
+//     i++
     
-    if (i >= window.innerWidth) {
-        i = -400
-    }
-})
+//     if (i >= window.innerWidth) {
+//         i = -400
+//     }
+// })
+
+// const bouton = document.querySelector("#bouton")
+
+
+const interval2 = setInterval(() => {
+    const x = Math.floor(Math.random() * window.innerWidth)
+    const y = Math.floor(Math.random() * window.innerHeight)
+
+    homer.style.top = `${y}px`
+    homer.style.right = `${x}px`
+}, 5500)
+
+
+
+// const bouton = document.querySelector("#bouton")
+
+// bouton.addEventListener("click", () => {
+//     const x = Math.floor(Math.random() * window.innerWidth)
+//     const y = Math.floor(Math.random() * window.innerHeight)
+
+//     homer.style.top = `${y}px`
+// })
 
